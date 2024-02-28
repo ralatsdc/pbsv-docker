a3654d7cd5c8b3fa4b45719585203fc408bf21df
Raymond LeClair <raymond.leclair@springbok.io>
2024-02-28 16:50:36 -0500

    Correct operation of update files

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 eval "$(conda shell.bash hook)"
 conda env create -f update.yaml
-conda activate pbsv-update
-conda env export > environment.yaml
+conda activate pbsv
+conda env export | grep -v prefix > environment.yaml
 conda deactivate
-conda env remove -n pbsv-update
+conda env remove -n pbsv
